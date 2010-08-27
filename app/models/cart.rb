1c83c5bce4b3717f2a8faed1004a2c027dbd248b
johnkors <johnkors@gmail.com>
2010-08-27 21:36:32 +0200

    Iteration C2 Page 107

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,14 @@ class Cart
     @items = []
   end
 
+
   def add_product(product)
-    @items << product
+    current_item = @items.find {|item| item.product == product}
+    if current_item
+      current_item.increment_quantity
+    else
+      @items << CartItem.new(product)
+    end
   end
+
 end
