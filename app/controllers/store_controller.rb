25656ceeb3cf645a3d8346333b07056557983984
johnkors <johnkors@gmail.com>
2010-08-27 22:16:52 +0200

    Iteration C3 Page 112

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,15 +9,27 @@ class StoreController < ApplicationController
     product = Product.find(params[:id])
     @cart = find_cart
     @cart.add_product(product)
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    flash[:notice] = "Invalid product"
+    redirect_to :action => 'index'
   end
+  
 
+
+
+  def empty_cart
+    session[:cart] = nil
+    flash[:notice] = "Your cart is currently empty"
+    redirect_to :action => 'index'
+  end
 
 
 private
+
   def find_cart
     session[:cart] ||= Cart.new
   end
-  
 
 
 end
