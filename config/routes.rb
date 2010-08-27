fb301bd91926b70734616a089492224ea6621dc7
johnkors <johnkors@gmail.com>
2010-08-27 18:28:48 +0200

    Iteration B1 - page 90

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :products
+  map.resources :stores
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
   map.root :controller => 'products'
